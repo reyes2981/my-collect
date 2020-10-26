ee17d5896be5190a8af0eb532f7df3a33a2fd542
reyes2981 <noe.reyes92@yahoo.com>
2020-10-26 18:36:35 -0500

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,9 @@
-
-
+def my_collect(array) # define a method with one argument 
+    i = 0 #set counter
+    new_array = [] #create new array
+    while i < array.length #while "i" is less than the length of new_array
+    new_array << yield(array[i]) #shovel block of code
+    i += 1 #add one and iterate
+    end
+    new_array #call method
+end
